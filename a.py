de577c3102bd3b17022c70bc6978b0d3d22f93a2
Senol ALDIBAS <senol@localhost.localdomain>
2020-06-02 13:26:12 +0300

    x --> o

--- a.py
+++ a.py
@@ -9,7 +9,7 @@ for i in xrange(0,height,1):
             sys.stdout.write(" ")
             sys.stdout.write("xxx")
             for k in xrange(0,space,1):
-                                                                                        sys.stdout.write("xxx")
+                                                                                        sys.stdout.write("OOO")
 
                                                                                                 for j in xrange(0,i,1):
                                                                                                                 sys.stdout.write(" ")
