a5ba4c4a82b2627df008fcdadfe87026237170a7
Senol ALDIBAS <senol@localhost.localdomain>
2020-06-02 11:40:38 +0300

    ex

---
+++ W.py
@@ -0,0 +1,25 @@
+import sys,os
+
+height = raw_input("Yukseklik Giriniz:  ")
+height = int(height)
+space = height*2-2
+for i in xrange(0,height,1):
+    for h in xrange(0,4,1):
+        for j in xrange(0,i,1):
+            sys.stdout.write(" ")
+    
+        sys.stdout.write("xxx")
+
+        for k in xrange(0,space,1):
+            sys.stdout.write(" ")
+        
+        sys.stdout.write("xxx")
+
+        for j in xrange(0,i,1):
+            sys.stdout.write(" ")
+
+
+
+    space = space - 2
+
+    sys.stdout.write("\n")
